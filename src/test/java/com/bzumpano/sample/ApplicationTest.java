6caf78c90cb98ade6ae47ec5523ace402ab40d4e
Bruno Zumpano Nakashima Miyahara <bzumpano@caiena.net>
2016-03-10 10:58:46 -0300

    refactor

--- src/test/java/com/bzumpano/sample/ApplicationTest.java
+++ src/test/java/com/bzumpano/sample/ApplicationTest.java
@@ -7,12 +7,12 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = GradleTddApplication.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-public class GradleTddApplicationTests {
+public class ApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
