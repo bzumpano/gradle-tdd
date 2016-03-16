79d046e399cc4569c75a32b8a6f52cbe85dc3770
Bruno Zumpano Nakashima Miyahara <bzumpano@caiena.net>
2016-03-16 16:50:02 -0300

    Setup H2 database and Flyway plugin

--- src/test/java/com/bzumpano/sample/ApplicationTest.java
+++ src/test/java/com/bzumpano/sample/ApplicationTest.java
@@ -1,15 +1,27 @@
 package com.bzumpano.sample;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class ApplicationTest {
+
+    @Autowired
+    private JdbcTemplate template;
+
+    @Test
+    public void testDefaultSettings() throws Exception {
+        assertEquals(this.template.queryForObject("SELECT COUNT(*) from PERSON", Integer.class).intValue(), 1);
+    }
 
     @Test
     public void contextLoads() {
