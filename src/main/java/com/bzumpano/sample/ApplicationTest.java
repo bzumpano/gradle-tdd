45b8c6238918ac7a0386ea09b5b8c60fdc9e1e54
Bruno Zumpano Nakashima Miyahara <bzumpano@caiena.net>
2016-03-17 17:43:55 -0300

    Fix test errors with asset-pipeline

---
+++ src/main/java/com/bzumpano/sample/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.bzumpano.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTest {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ApplicationTest.class, args);
+    }
+}
