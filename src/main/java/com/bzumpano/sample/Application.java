ada022f5c853c2bbffb0322670ad0681dedb2d25
Bruno Zumpano Nakashima Miyahara <bzumpano@caiena.net>
2016-03-18 17:08:32 -0300

    Refactor

--- src/main/java/com/bzumpano/sample/Application.java
+++ src/main/java/com/bzumpano/sample/Application.java
@@ -4,9 +4,5 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.bzumpano.sample","asset.pipeline.springboot"})
-public class Application {
-
-    public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+public class Application extends ApplicationBase {
 }
