239e5675fa07fdd59f256956866cd5450b75d2d5
bzumpano <bzumpano@caiena.net>
2017-02-24 19:13:30 -0300

    Fix tests

--- src/test/java/com/bzumpano/sample/ProjectTest.java
+++ src/test/java/com/bzumpano/sample/ProjectTest.java
@@ -3,16 +3,14 @@ package com.bzumpano.sample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ApplicationTests.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProjectTest {
 
     @Autowired
--- src/test/java/com/bzumpano/sample/controller/HomeControllerTest.java
+++ src/test/java/com/bzumpano/sample/controller/HomeControllerTest.java
@@ -1,16 +1,40 @@
 package com.bzumpano.sample.controller;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 
 /**
  * Created by bzumpano on 3/21/16.
  */
-public class HomeControllerTest extends BaseControllerTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
+public class HomeControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    protected MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).
+                apply(SecurityMockMvcConfigurers.springSecurity()).
+                build();
+    }
 
     @Test
     public void verifiesHomePageLoads() throws Exception {
